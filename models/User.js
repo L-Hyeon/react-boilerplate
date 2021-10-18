99460d7ea95cc604a76b4b8bb0d0fa57d1666dfe
L <dkh1001@outlook.kr>
2021-10-18 10:21:55 +0900

    Add password encrypt function

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+// 암호키인 salt의 길이 설정
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name: {
@@ -23,5 +27,27 @@ const userSchema = mongoose.Schema({
     tokenExp: Number
 })
 
+//가져온 user 데이터를 저장하기 전 비밀번호 암호화
+userSchema.pre('save', function( next ){
+    var user = this
+
+    //password 필드가 변환될 때만 암호화
+    //=> 다른 필드 수정 시 재암호화 되는 현상 방지
+    if (user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err)
+    
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err)
+    
+                user.password = hash
+                next()
+            })
+        })
+    }
+
+})
+
+// 메인 js 파일에서 사용하기 위해 export
 const User = mongoose.model('User', userSchema)
 module.exports = { User }
